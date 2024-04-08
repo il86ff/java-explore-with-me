a486666004be831a38f42beb3e40d1c8460b7d99
il86ff <il86ff@gmail.com>
2024-04-08 23:28:08 +0300

    feat: statistics

--- statistics/server/src/main/java/ru/practicum/controller/ServerController.java
+++ statistics/server/src/main/java/ru/practicum/controller/ServerController.java
@@ -1,9 +1,50 @@
 package ru.practicum.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.RequestDTO;
+import ru.practicum.RequestOutDTO;
+import ru.practicum.service.StatService;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class ServerController {
+    private static final DateTimeFormatter DTF = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    private final StatService statService;
+
+    @PostMapping("/hit")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public void addRequest(@Valid @RequestBody RequestDTO requestDto) {
+        statService.addRequest(requestDto);
+    }
+
+    @GetMapping("/stats")
+    public ResponseEntity<List<RequestOutDTO>> getStats(@RequestParam String start,
+                                                        @RequestParam String end,
+                                                        @RequestParam(required = false) List<String> uris,
+                                                        @RequestParam(defaultValue = "false") Boolean unique) {
+
+        LocalDateTime startDT;
+        LocalDateTime endDT;
+        try {
+            startDT = LocalDateTime.parse(start, DTF);
+            endDT = LocalDateTime.parse(end, DTF);
+        } catch (DateTimeParseException e) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        List<RequestOutDTO> results = statService.getRequestsWithViews(startDT, endDT, uris, unique);
+        return ResponseEntity.ok().body(results);
+    }
 }
--- statistics/server/src/main/java/ru/practicum/repository/ApplicationRepository.java
+++ statistics/server/src/main/java/ru/practicum/repository/ApplicationRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.model.Application;
 
 import java.util.Optional;
+
 public interface ApplicationRepository extends JpaRepository<Application, Integer> {
     Optional<Application> findByName(String app);
 }
