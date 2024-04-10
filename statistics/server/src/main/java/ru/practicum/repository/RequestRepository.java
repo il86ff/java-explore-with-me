5e9f2ed8def8d3722d410687d0a78655e571d56e
il86ff <il86ff@gmail.com>
2024-04-10 22:21:28 +0300

    fix: sql

--- statistics/server/src/main/java/ru/practicum/repository/RequestRepository.java
+++ statistics/server/src/main/java/ru/practicum/repository/RequestRepository.java
@@ -9,35 +9,35 @@ import java.util.List;
 
 public interface RequestRepository extends JpaRepository<Request, Integer> {
 
-    @Query(value = "SELECT new ru.practicum.RequestOutDto(a.name, r.uri, COUNT(r.ip)) " +
+    @Query(value = "SELECT new ru.practicum.RequestOutDTO(a.name, r.uri, COUNT(r.ip)) " +
             "FROM Request as r " +
-            "LEFT JOIN App as a ON a.id = r.app.id " +
+            "LEFT JOIN Application as a ON a.id = r.app.id " +
             "WHERE r.timestamp between ?1 AND ?2 " +
             "AND r.uri IN (?3) " +
             "GROUP BY a.name, r.uri " +
             "ORDER BY COUNT(r.ip) DESC ")
     List<RequestOutDTO> getAllRequestsWithUri(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query(value = "SELECT new ru.practicum.RequestOutDto(a.name, r.uri, COUNT(DISTINCT r.ip)) " +
+    @Query(value = "SELECT new ru.practicum.RequestOutDTO(a.name, r.uri, COUNT(DISTINCT r.ip)) " +
             "FROM Request as r " +
-            "LEFT JOIN App as a ON a.id = r.app.id " +
+            "LEFT JOIN Application as a ON a.id = r.app.id " +
             "WHERE r.timestamp between ?1 AND ?2 " +
             "AND r.uri IN (?3) " +
             "GROUP BY a.name, r.uri " +
             "ORDER BY COUNT(DISTINCT r.ip) DESC ")
     List<RequestOutDTO> getUniqueIpRequestsWithUri(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query(value = "SELECT new ru.practicum.RequestOutDto(a.name, r.uri, COUNT(DISTINCT r.ip)) " +
+    @Query(value = "SELECT new ru.practicum.RequestOutDTO(a.name, r.uri, COUNT(DISTINCT r.ip)) " +
             "FROM Request as r " +
-            "LEFT JOIN App as a ON a.id = r.app.id " +
+            "LEFT JOIN Application as a ON a.id = r.app.id " +
             "WHERE r.timestamp between ?1 AND ?2 " +
             "GROUP BY a.name, r.uri " +
             "ORDER BY COUNT(DISTINCT r.ip) DESC ")
     List<RequestOutDTO> getUniqueIpRequestsWithoutUri(LocalDateTime start, LocalDateTime end);
 
-    @Query(value = "SELECT new ru.practicum.RequestOutDto(a.name, r.uri, COUNT(r.ip)) " +
+    @Query(value = "SELECT new ru.practicum.RequestOutDTO(a.name, r.uri, COUNT(r.ip)) " +
             "FROM Request as r " +
-            "LEFT JOIN App as a ON a.id = r.app.id " +
+            "LEFT JOIN Application as a ON a.id = r.app.id " +
             "WHERE r.timestamp between ?1 AND ?2 " +
             "GROUP BY a.name, r.uri " +
             "ORDER BY COUNT(r.ip) DESC ")
