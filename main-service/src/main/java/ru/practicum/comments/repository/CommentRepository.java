80090f04a13ceaf27dc048a5be451a87e7966d17
il86ff <il86ff@gmail.com>
2024-04-20 11:52:05 +0300

    fix: comments checkstyle

--- main-service/src/main/java/ru/practicum/comments/repository/CommentRepository.java
+++ main-service/src/main/java/ru/practicum/comments/repository/CommentRepository.java
@@ -7,6 +7,7 @@ import ru.practicum.comments.entity.Comment;
 
 import java.util.List;
 import java.util.Optional;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
