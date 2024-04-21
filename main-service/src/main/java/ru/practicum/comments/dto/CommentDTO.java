d1f549753a83d106037dc41b8819380b30b85867
il86ff <il86ff@gmail.com>
2024-04-21 21:14:34 +0300

    fix: wrong annotations on CommentDTO fields

--- main-service/src/main/java/ru/practicum/comments/dto/CommentDTO.java
+++ main-service/src/main/java/ru/practicum/comments/dto/CommentDTO.java
@@ -1,13 +1,11 @@
 package ru.practicum.comments.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.practicum.events.dto.EventShortDTO;
 import ru.practicum.users.dto.UserShortDTO;
 
-import javax.persistence.Column;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
@@ -21,11 +19,10 @@ public class CommentDTO {
     @Size(min = 5, max = 5000)
     private String text;
 
-    @Column(name = "created_on")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdOn;
 
     private UserShortDTO user;
-    
+
     private EventShortDTO event;
+
 }
